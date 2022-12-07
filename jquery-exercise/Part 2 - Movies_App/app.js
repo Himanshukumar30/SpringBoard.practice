21e7854b635553d776d5e7ac110b54f67059f09f
himanshukumar30 <himanshuk.kumar01@gmail.com>
2022-12-06 19:24:48 -0800

    adding movies-app

---
+++ jquery-exercise/Part 2 - Movies_App/app.js
@@ -0,0 +1,25 @@
+// Add new entry to movie container once click on submit
+$("form").on("submit", function (e) {
+  e.preventDefault();
+  let movieName = $("input").eq(0).val();
+  let movieRating = $("input").eq(1).val();
+  let newEntry = { movieName, movieRating };
+  let htmlToAdd = createNewEntry(newEntry);
+  $("#movie-container").append(htmlToAdd);
+  $("#new-movie-form").trigger("reset");
+});
+
+// Delete entry when clicked on delete button corresponding to movie name
+$("div").on("click", ".delete", function (evt) {
+  //   console.log(evt);
+  $(evt.target).parent().remove();
+});
+
+// function to create html to be pushed every time there is new Entry
+function createNewEntry(data) {
+  return `<div class=entry>
+        <p>${data.movieName}</p>
+        <p>${data.movieRating}</p>
+        <button class=delete>Delete</button>
+    </div>`;
+}
