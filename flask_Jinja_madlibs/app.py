aa8b334abea2b96e369d008dd1172246160c625f
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-01-02 17:20:20 -0800

    adding story selection in flask Jinja exercise

--- flask_Jinja_madlibs/app.py
+++ flask_Jinja_madlibs/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from stories import story
+from stories import stories
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -8,12 +8,25 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 app.debug=True
 toolbar = DebugToolbarExtension(app)
 
-@app.route('/')
-def questions():
+@app.route("/")
+def story_select():
+    return render_template("story-select.html", stories=stories.values())
+
+
+@app.route("/questions")
+def ask_questions():
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
     prompts = story.prompts
-    return render_template('questions.html', prompts=prompts)
+
+    return render_template("questions.html",story_id=story_id,title=story.title,prompts=prompts)
 
 @app.route("/story")
 def display_story():   
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
     text = story.generate(request.args)
-    return render_template("story.html", text=text)
+
+    return render_template("story.html",title=story.title,text=text)
